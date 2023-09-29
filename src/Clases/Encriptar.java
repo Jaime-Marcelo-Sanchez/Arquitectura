5cae14096c58bf56973c4d2fff6a8e8307338704
Jaime-Marcelo-Sanchez <JMarcelo.SanchezIFW@gmail.com>
2023-09-28 21:41:15 -0500

    Clase encriptar

---
+++ src/Clases/Encriptar.java
@@ -0,0 +1,12 @@
+package Clases;
+
+public class Encriptar {
+
+    public String encriptarClave(String clave) {
+        return clave; // saplsj=2
+    }
+
+    public String desencriptarClave(String clave) {
+        return clave;
+    }
+}
