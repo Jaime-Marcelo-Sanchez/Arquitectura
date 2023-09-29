e23214aaeec2043463d393ad91bfc6c7055879dc
EmaDeivid <davidemanuelrojasjimenez@gmail.com>
2023-09-28 23:21:29 -0500

    no hay nada

--- src/Clases/Encriptar.java
+++ src/Clases/Encriptar.java
@@ -1,12 +1,39 @@
 package Clases;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 public class Encriptar {
 
+    private static final String claveS = "HolaMundoAccesoDisponible"; // 
+
     public String encriptarClave(String clave) {
-        return clave; // saplsj=2
+        try {
+            SecretKeySpec claveSecreta = new SecretKeySpec(claveS.getBytes(), "AES");
+            Cipher cifrador = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cifrador.init(Cipher.ENCRYPT_MODE, claveSecreta);
+
+            byte[] textoEncriptado = cifrador.doFinal(clave.getBytes());
+            return Base64.getEncoder().encodeToString(textoEncriptado);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public String desencriptarClave(String clave) {
-        return clave;
+    public String desencriptarClave(String claveEncriptada) {
+        try {
+            SecretKeySpec claveSecreta = new SecretKeySpec(claveS.getBytes(), "AES");
+            Cipher cifrador = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cifrador.init(Cipher.DECRYPT_MODE, claveSecreta);
+
+            byte[] textoDesencriptado = cifrador.doFinal(Base64.getDecoder().decode(claveEncriptada));
+            return new String(textoDesencriptado);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
+
