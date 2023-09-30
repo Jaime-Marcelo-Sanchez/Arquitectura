73da18ed999996159310f2cb7f1188ac4f73d6c6
Jaime-Marcelo-Sanchez <JMarcelo.SanchezIFW@gmail.com>
2023-09-29 22:12:35 -0500

    Error corregido
    De la nada se movio todo de la clase Encriptar y se agregaron cosas, pero bueno ya esta solucionado

--- src/Clases/Encriptar.java
+++ src/Clases/Encriptar.java
@@ -1,18 +1,13 @@
 package Clases;
 
-<<<<<<< HEAD
 import java.security.MessageDigest;
 import java.util.Arrays;
-import java.util.Base64;
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-=======
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
->>>>>>> e23214aaeec2043463d393ad91bfc6c7055879dc
 
 public class Encriptar {
+
     private static final String LLAVE = "Admin2091";
 
     public SecretKeySpec crearClave(String llave) {
@@ -28,11 +23,8 @@ public class Encriptar {
         }
     }
 
-    private static final String claveS = "HolaMundoAccesoDisponible"; // 
-
     public String encriptarClave(String clave) {
         try {
-<<<<<<< HEAD
             SecretKeySpec secretKeySpec = crearClave(LLAVE);
             Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
@@ -44,23 +36,11 @@ public class Encriptar {
         } catch (Exception e) {
             e.printStackTrace();
             return "";
-=======
-            SecretKeySpec claveSecreta = new SecretKeySpec(claveS.getBytes(), "AES");
-            Cipher cifrador = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cifrador.init(Cipher.ENCRYPT_MODE, claveSecreta);
-
-            byte[] textoEncriptado = cifrador.doFinal(clave.getBytes());
-            return Base64.getEncoder().encodeToString(textoEncriptado);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
->>>>>>> e23214aaeec2043463d393ad91bfc6c7055879dc
         }
     }
 
     public String desencriptarClave(String claveEncriptada) {
         try {
-<<<<<<< HEAD
             SecretKeySpec secretKeySpec = crearClave(LLAVE);
             Cipher cipher = Cipher.getInstance("AES");
             cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
@@ -71,18 +51,6 @@ public class Encriptar {
         } catch (Exception e) {
             e.printStackTrace();
             return "";
-=======
-            SecretKeySpec claveSecreta = new SecretKeySpec(claveS.getBytes(), "AES");
-            Cipher cifrador = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cifrador.init(Cipher.DECRYPT_MODE, claveSecreta);
-
-            byte[] textoDesencriptado = cifrador.doFinal(Base64.getDecoder().decode(claveEncriptada));
-            return new String(textoDesencriptado);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
->>>>>>> e23214aaeec2043463d393ad91bfc6c7055879dc
         }
     }
 }
-
